066ae52ec12471e7c8e6dc670f401bdfef30a760
Posmangiu Ionut <123485676+WildPotato151@users.noreply.github.com>
2024-05-20 17:24:17 +0300

    Metode noi

--- Clase/Componenta.cpp
+++ Clase/Componenta.cpp
@@ -18,6 +18,102 @@ Componenta::Componenta(int id, string tip, string brand, string model, string sp
     this->pret = pret;
     this->stock = stock;
 
-
 }
 Componenta::~Componenta() {}
+
+string Componenta::Info() const {
+
+    string stock_string = (stock ? "Da" : "Nu");
+
+    string info = "ID: " + (id == -1 ? "N/A" : to_string(id)) + "\n";
+    info += "Tip: " + tip + "\n";
+    info += "Brand: " + brand + "\n";
+    info += "Model: " + model + "\n";
+    info += "Specificatii: " + specs + "\n";
+    info += "Pret: " + to_string(pret) + "\n";
+    info += "In stock: " + stock_string + "\n";
+
+}
+
+string Componenta::ConversieToString_FisierCSV() const {
+
+    string output = to_string(id) + SEPARATOR_FISIER + tip + SEPARATOR_FISIER + brand + SEPARATOR_FISIER +
+        model + SEPARATOR_FISIER + specs + SEPARATOR_FISIER + to_string(pret) + SEPARATOR_FISIER +
+        (stock ? "1" : "0") + SEPARATOR_FISIER;
+
+    return output;
+}
+
+string Componenta::selecteazaTipComponenta() {
+
+    int optiune = -1;
+    string tip;
+
+    cout<<"Selecteaza tipul de componenta: "<<endl;
+    cout<<"1. CPU "<<endl;
+    cout<<"2. GPU "<<endl;
+    cout<<"3. Placa de baza"<<endl;
+    cout<<"4. Memorie "<<endl;
+    cout<<"5. SSD"<<endl;
+    cout<<"6. Sursa"<<endl;
+    cout<<"7. Carcasa"<<endl;
+
+    while (optiune < 1 || optiune > 6) {
+        cin >> optiune;
+
+        switch (optiune) {
+            case 1:
+                tip = "CPU";
+            break;
+            case 2:
+                tip = "GPU";
+            break;
+            case 3:
+                tip = "Placa de baza";
+            break;
+            case 4:
+                tip = "Memorie";
+            break;
+            case 5:
+                tip = "SSD";
+            break;
+            case 6:
+                tip = "Sursa";
+            break;
+            case 7:
+                tip = "Carcasa";
+            break;
+            default:
+                cout << "Optiune invalida!!! Selecteazsa o optiune valida... ";
+            break;
+        }
+    }
+    return tip;
+}
+
+bool Componenta::selecteazaStockComponenta() {
+
+    int optiune = -1;
+    bool stock;
+
+    cout << "Selecteaza daca componenta se afla sau nu in stock:  " << endl;
+    cout << "1. In stock" << endl;
+    cout << "2. Nu se afla in stock" << endl;
+    cout << "Introduceti o optiune... ";
+    while (optiune < 1 || optiune > 2) {
+        cin >> optiune;
+
+        switch (optiune) {
+            case 1:
+                stock = true;
+            break;
+            case 2:
+                stock = false;
+            break;
+            default:
+                cout << "Optiune invalida!!! Selecteazsa o optiune valida... ";
+            break;
+        }
+    }
+    return optiune;
+}
--- Clase/Componenta.h
+++ Clase/Componenta.h
@@ -14,7 +14,7 @@ using namespace std;
 class Componenta
 {
 private:
-    char SEPARATOR_FISIER = ';';
+    char SEPARATOR_FISIER = ',';
 
     int id;
     string tip;
@@ -26,8 +26,31 @@ private:
 
 public:
     Componenta(int id = -1, string tip = "N/A", string brand = "N/A", string model = "N/A", string specs = "N/A", float pret = -1, bool stock = false );
-
     ~Componenta();
+
+    int getID() const {return id;}
+    string getTip() const {return tip;}
+    string getBrand() const {return brand;}
+    string getModel() const {return model;}
+    string getSpecs() const {return specs;}
+    string getPret() const {return to_string(pret);}
+    bool esteStock() const {return stock;}
+
+    void setId(int newId) { id = newId; }
+    void setTip(const string newTip) { tip = newTip; }
+    void setBrand(const string newBrand) { brand = newBrand; }
+    void setModel(const string newModel) { model = newModel; }
+    void setSpecs(const string newSpecs) { specs = newSpecs; }
+    void setPret(float newPret) { pret = newPret; }
+    void setStock(bool newStock) { stock = newStock; }
+
+    string ConversieToString_FisierCSV() const;
+
+    static string selecteazaTipComponenta();
+    static bool selecteazaStockComponenta();
+
+
+    string Info() const;
 };
 
 
